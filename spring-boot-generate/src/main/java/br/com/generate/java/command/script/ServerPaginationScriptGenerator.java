aa693ac8481d8e43666276ddb5dd677f3608d0a8
Shivakumar <contactniranjan366@gmail.com>
2018-01-14 22:43:36 +0530

    Changed file name

--- spring-boot-generate/src/main/java/br/com/generate/java/command/script/ServerPaginationScriptGenerator.java
+++ spring-boot-generate/src/main/java/br/com/generate/java/command/script/ServerPaginationScriptGenerator.java
@@ -17,12 +17,12 @@ public ServerPaginationScriptGenerator (String nameClass, String parametersClass
 	}
 	
 	public void generatescript(String nameClass , String parameterClass) throws IOException{
-		String scriptString = IOUtils.toString(getClass().getResourceAsStream("/templates/template-client-script.txt"), null);
+		String scriptString = IOUtils.toString(getClass().getResourceAsStream("/templates/template-server-script.txt"), null);
 
 		String classNameParam = nameClass;
 		String paramClassName = nameClass.toLowerCase();
 		String pathUrl = "/" + nameClass.toLowerCase() + "s";
-		String tdAttributes = generateScriptparameters(nameClass, parameterClass);
+		String tdAttributes = generateServerScriptParameters(nameClass, parameterClass);
 		scriptString = scriptString.replace("paramClassName", paramClassName);
 		scriptString= scriptString.replace("${td_attributes}", tdAttributes);
 		File newScriptFile = new File(getUserDir() + "/src/main/webapp/page_js/" + paramClassName+".js");
